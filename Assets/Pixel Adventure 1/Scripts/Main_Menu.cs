3f95f6fdf1b6d833a2c9e9fa0b4ca1c2cbcf8265
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-08-31 13:15:38 -0700

    Added Main Menu and following
    Added Main menu and font and button setup and added code behavior added restart button to panel and added a end panel .....

---
+++ Assets/Pixel Adventure 1/Scripts/Main_Menu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Main_Menu : MonoBehaviour
+{
+    [SerializeField]
+    private Button Start;
+    [SerializeField]
+    private Button Quit;
+    [SerializeField]
+    private Button Level_1;
+    [SerializeField]
+    private Button Level_2;
+    [SerializeField]
+    private Button Level_3;
+
+
+    private void OnEnable()
+    {
+        Start.onClick.AddListener(() => buttonCallBack(Start));
+        Quit.onClick.AddListener(() => buttonCallBack(Quit));
+        Level_1.onClick.AddListener(() => buttonCallBack(Level_1) );
+        Level_2.onClick.AddListener(() => buttonCallBack(Level_2) );
+        Level_3.onClick.AddListener(() => buttonCallBack(Level_3) );
+    }
+
+   
+    private void buttonCallBack(Button ButtonPressed)
+    {
+        if(ButtonPressed == Start)
+        {
+            SceneManager.LoadScene(1);
+            Time.timeScale = 1.0f;
+        }
+        else if(ButtonPressed == Quit)
+        {
+            Application.Quit();
+        }
+        else if(ButtonPressed == Level_1)
+        {
+            SceneManager.LoadScene(1);
+            Time.timeScale = 1.0f;
+        }
+        else if (ButtonPressed == Level_2)
+        {
+            Debug.Log("Level - 2");
+        }
+        else if (ButtonPressed == Level_3)
+        {
+            Debug.Log("Level - 3");
+        }
+    }
+}
--- Assets/Pixel Adventure 1/Scripts/UI_Manager.cs
+++ Assets/Pixel Adventure 1/Scripts/UI_Manager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UI_Manager : MonoBehaviour
 {
@@ -8,6 +10,16 @@ public class UI_Manager : MonoBehaviour
     private Animator _animator;
     [SerializeField]
     private GameObject _Pause_Panel;
+    [SerializeField]
+    private Button Resume;
+    [SerializeField]
+    private Button Main;
+    [SerializeField]
+    private Button Quit;
+    [SerializeField]
+    private Button Pause_Button;
+    [SerializeField]
+    private Button Restart;
 
     private static UI_Manager _instance;
     public static UI_Manager Instance
@@ -26,17 +38,42 @@ public class UI_Manager : MonoBehaviour
     {
         _instance = this;
         _animator.updateMode = AnimatorUpdateMode.UnscaledTime;
+        //Scene ActiveScene = SceneManager.GetActiveScene();
     }
+    private void OnEnable()
+    {
+        Resume.onClick.AddListener(() => buttonCallBack(Resume));
+        Pause_Button.onClick.AddListener(() => buttonCallBack(Pause_Button));
+        Main.onClick.AddListener(() => buttonCallBack(Main));
+        Restart.onClick.AddListener(() => buttonCallBack(Restart));
+        Quit.onClick.AddListener(() => buttonCallBack(Quit));
 
-    public void Pause_Button()
-    {
-        _Pause_Panel.SetActive(true);
-        _animator.SetBool("IsPause", true);
-        Time.timeScale = 0.0f;
     }
-    public void Resume_Button()
+    private void buttonCallBack(Button ButtonPressed)
     {
-        _Pause_Panel.SetActive(false);
-        Time.timeScale = 1.0f;
+        if(ButtonPressed == Resume)
+        {
+            _Pause_Panel.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+        else if(ButtonPressed == Pause_Button)
+        {
+            _Pause_Panel.SetActive(true);
+            _animator.SetBool("IsPause", true);
+            Time.timeScale = 0.0f;
+        }
+        else if(ButtonPressed == Main)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else if(ButtonPressed == Restart)
+        {
+            Time.timeScale = 1.0f;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if(ButtonPressed == Quit)
+        {
+            Application.Quit();
+        }
     }
 }
