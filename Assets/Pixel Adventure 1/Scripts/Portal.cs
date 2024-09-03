fa46df3856b08f502f1d934d0569dc37dff1426a
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-09-03 14:33:07 -0700

    level 3 completed
    Added all the level 3 remaining design, added trampoline ... and portal .... added all the apples and completed the level 3 design

---
+++ Assets/Pixel Adventure 1/Scripts/Portal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.position = new Vector3(-0.6f,28,0);
+        }
+        
+    }
+}
